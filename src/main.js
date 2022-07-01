5167982aa3c1dbfe8fa6efd5b702bdabc846af5c
SAX_94 <matteosacchi94@gmail.com>
2022-07-01 14:05:29 +0200

    imported useful entities

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faMoneyBill1 } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faEye } from '@fortawesome/free-regular-svg-icons'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faFacebookF, faTwitter, faInstagram, faMagnifyingGlass, faUserPlus, faCalendarDays, faPlus, faMoneyBill1, faUser, faEye, faCaretDown)
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
