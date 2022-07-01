cd3189aa16ee7a607270fdd05e961d81f24f9a1a
SAX_94 <matteosacchi94@gmail.com>
2022-07-01 13:43:57 +0200

    imported correct font

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap');
 #app {
-  @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap');
   font-family: 'Nunito', sans-serif;
 }
 </style>
