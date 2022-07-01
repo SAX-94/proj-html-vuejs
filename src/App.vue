3ddaee6074a2c25325038c672620cc4c8aeb1ffa
SAX_94 <matteosacchi94@gmail.com>
2022-07-01 14:13:06 +0200

    css style reset added

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,11 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap');
 #app {
   font-family: 'Nunito', sans-serif;
