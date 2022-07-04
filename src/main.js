730625c190d62c290628c6e3af4635f424d18549
SAX_94 <matteosacchi94@gmail.com>
2022-07-04 22:42:44 +0200

    added Bootstrap to make page responsive

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
